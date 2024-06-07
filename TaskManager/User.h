9eae1566d8e241dabd0a4c3925609d1e20226e39
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-06-07 18:54:36 +0300

    Add readUser function implementation

--- TaskManager/User.h
+++ TaskManager/User.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "MyString.h"
 
-class User
+// possibly change back to class
+struct User
 {
 	MyString username;
 	MyString password;
-	
 };
--- TaskManager/UserSerializer.cpp
+++ TaskManager/UserSerializer.cpp
@@ -2,13 +2,23 @@
 
 #include "UserSerializer.h"
 #include "MyString.h"
+#include "ExceptionMessages.h"
 
 void UserSerializer::saveUser(const User& user, const char* fileName)
 {
-	std::ofstream ofs(fileName);
+	std::ofstream file(fileName);
 
-	if (true)
+	if (!file.is_open())
 	{
-
+		throw std::runtime_error(ExceptionMessages::FILE_OPEN_WRITE_ERROR);
 	}
+
+	size_t usernameLength = user.username.getSize();
+	size_t passwordLength = user.password.getSize();
+
+	file.write(reinterpret_cast<const char*>(&usernameLength), sizeof(usernameLength));
+	file.write(user.username.c_str(), usernameLength);
+
+	file.write(reinterpret_cast<const char*>(&passwordLength), sizeof(passwordLength));
+	file.write(user.password.c_str(), passwordLength);
 }

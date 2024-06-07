75f94e29e5a57aa1f8dfb9bc8f6341608a76c92d
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-06-07 16:50:48 +0300

    Update statusToStringFunction

--- TaskManager/Problem.cpp
+++ TaskManager/Problem.cpp
@@ -2,16 +2,23 @@
 
 int Problem::lastUid = 0;
 
-MyString statusToString(Status status) const
+MyString Problem::statusToString(Status status) const
 {
 	switch (status)
 	{
-	case ON_HOLD: return "ON_HOLD";
+	case Status::ON_HOLD:
+		return "ON_HOLD";
 		break;
-	case IN_PROCESS: return "IN_PROCESS";
+	case Status::IN_PROCESS:
+		return "IN_PROCESS";
 		break;
-	case DONE: return "DONE";
-	case OVERDUE: return "OVERDUE";
+	case Status::DONE: return
+		"DONE";
+		break;
+	case Status::OVERDUE:
+		return "OVERDUE";
+		break;
 	default: return "UNKNOWN";
+		break;
 	}
 }

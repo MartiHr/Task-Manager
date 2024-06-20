4b8f8a76612771d0203e6f41c06d1108fe64fb47
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-06-20 16:39:06 +0300

    Make the vector heterogenous

--- TaskManager/Dashboard.h
+++ TaskManager/Dashboard.h
@@ -7,6 +7,7 @@ class Dashboard
 {
 private:
 	Vector<Task> tasksForToday;
+
 	std::time_t today;
 
     bool isTaskDueToday(const Task& task);
--- TaskManager/TaskManager.cpp
+++ TaskManager/TaskManager.cpp
@@ -140,7 +140,7 @@ void TaskManager::handleLogin(std::istream& is)
 
 			// load the dashboard of the user
 			//dashboard.setTasks(loginUsername);
-
+			dashboard.setTasks()
 			break;
 		}
 		else

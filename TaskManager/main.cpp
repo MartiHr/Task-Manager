af278f337309d5eb3aeea69e774a637fa741adcc
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-06-23 23:09:03 +0300

    Update main.cpp

--- TaskManager/main.cpp
+++ TaskManager/main.cpp
@@ -9,43 +9,5 @@
 
 int main()
 {
-	/*User u = { "george", "123123" };
-	User u2 = { "2", "2" };
-
-	UserSerializer::saveUser(u, "user.bin");
-
-	User newU;
-	UserSerializer::readUser("user.bin");
-	newU = UserSerializer::readUser("user.bin");
-
-	std::cout << newU.username << newU.password;*/
-
-	//TaskManager::start(std::cin, "users.bin");
-
-	/*MyString str;
-	std::cin >> str;*/
-
-	//Optional<std::time_t> time = 
-
-	/*Vector<int> arr;
-	arr.pushBack(1);
-	arr.pushBack(2);
-	arr.pushBack(3);
-	arr.pushBack(4);
-	arr.pushBack(5);
-
-	for (int i = 0; i < arr.getSize(); i++)
-	{
-		if (i == 3)
-		{
-			arr.popAt(i);
-		}
-	}
-
-	for (int i = 0; i < arr.getSize(); i++)
-	{
-		std::cout << arr[i] << std::endl;
-	}*/
-
 	TaskManager::start(std::cin, "users.bin");
 }
